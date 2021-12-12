efb2d8aaade75d5ac5030131ba85be68de9f3774
Miguel Ruiz <mig.ruiz@gmail.com>
2021-12-12 18:55:01 +0000

    testing all good

--- App/app.js
+++ App/app.js
@@ -2,29 +2,42 @@ const { Observable,merge,timer } = require('rxjs');
 const { mergeMap, map,share,filter,mapTo,take,debounceTime,throttle,throttleTime} = require('rxjs/operators');
 var mqtt = require('./mqttCluster.js');
 
-global.mtqqLocalPath = process.env.MQTTLOCAL;
-//global.mtqqLocalPath = 'mqtt://piscos.tk';
+//global.mtqqLocalPath = process.env.MQTTLOCAL;
+global.mtqqLocalPath = 'mqtt://piscos.tk';
 
 
 const KEEPLIGHTONFORSECS = 30 * 1000
-//const STARTINGFROMHOURS = 8
-//const ENDINGATHOURS = 17
-const STARTINGFROMHOURS = process.env.STARTINGFROMHOURS
-const ENDINGATHOURS = process.env.ENDINGATHOURS
+const STARTINGFROMHOURS = 8
+const ENDINGATHOURS = 17
+//const STARTINGFROMHOURS = process.env.STARTINGFROMHOURS
+//const ENDINGATHOURS = process.env.ENDINGATHOURS
 
+//const DOOR_SENSOR_TOPIC = process.env.DOOR_SENSOR_TOPIC
+const DOOR_SENSOR_TOPIC = 'rflink/EV1527-001c4e'
 
+//const OUTDOOR_SENSOR_TOPIC = process.env.OUTDOOR_SENSOR_TOPIC
+const OUTDOOR_SENSOR_TOPIC = 'rflink/EV1527-0a3789'
 
 
 console.log(`starting entrance lights current time ${new Date()}`)
-const movementSensorsReadingStream = new Observable(async subscriber => {  
+
+const doorEntranceSensor = new Observable(async subscriber => {  
     var mqttCluster=await mqtt.getClusterAsync()   
-    mqttCluster.subscribeData('EV1527', function(content){
-        if ((content.ID==='001c4e') || content.ID==='0a3789'){
-            console.log(content.ID);
-            subscriber.next({data:'16340250'})
-        }
+    mqttCluster.subscribeData(DOOR_SENSOR_TOPIC, function(content){        
+            subscriber.next({content})
     });
 });
+
+const outdoorSensor = new Observable(async subscriber => {  
+    var mqttCluster=await mqtt.getClusterAsync()   
+    mqttCluster.subscribeData(OUTDOOR_SENSOR_TOPIC, function(content){        
+            subscriber.next({content})
+    });
+});
+
+const movementSensorsReadingStream = merge(doorEntranceSensor,outdoorSensor)
+
+
 
 const sharedSensorStream = movementSensorsReadingStream.pipe(
     filter(_ => new Date().getHours() < STARTINGFROMHOURS || new Date().getHours() >= ENDINGATHOURS),
@@ -43,6 +56,7 @@ const turnOnStream = sharedSensorStream.pipe(
 
 merge(turnOnStream,turnOffStream)
 .subscribe(async m => {
+    console.log(m);
     (await mqtt.getClusterAsync()).publishMessage('esp/front/door/light',m)
 })
 
